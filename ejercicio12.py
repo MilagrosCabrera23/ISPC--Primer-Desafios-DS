f78fc8c79f07e40fdad0133005c65427b9ec4594
Milagros Cabrera <maguicabrera23@gmail.com>
2024-05-22 16:12:54 -0300

    first commit

---
+++ ejercicio12.py
@@ -0,0 +1,3 @@
+# 12- De lo anterior,podemos inferir que podemos especificar funciones que admitan argumentos unicamente posicionales o bien funciones que admitan docuemntos unicamente con palabras claves. Escribe un ejemplo de cada uno y luego intenta romper la regla especificada en la definicion de la funcion. 
+
+
